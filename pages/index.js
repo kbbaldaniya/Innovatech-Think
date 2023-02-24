e5218af65a6512dae5c0596131210ec0d2432ba9
kbbaldaniya <kishanbaldaniya2@gmail.com>
2023-02-24 12:57:56 +0530

    Change for test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div>
-        <h1>
-          Hey, User
+        <h1 className="text-9xl">
+          Hi,
+          Welcome to my github pages action
         </h1>
     </div>
   )
