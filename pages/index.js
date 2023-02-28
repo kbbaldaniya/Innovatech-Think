22d79e0f685f25ec681a3788457f39e216239a7f
kbbaldaniya <kishanbaldaniya2@gmail.com>
2023-02-28 13:04:05 +0530

    add data in route

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
-  const handleProps = () => { window.ReactNativeWebView.postMessage(JSON.stringify(data)) }
+  const handleProps = () => { window.ReactNativeWebView.postMessage(JSON.stringify({label: 'react-testing'})) }
   return (
     <div>
+      <h1>Native Testing...</h1>
       <button onClick={handleProps}>Click here</button>
     </div>
   )
