abca3a3598d2c1fa6d9c79629f34baaa11c75b72
kbbaldaniya <kishanbaldaniya2@gmail.com>
2023-03-01 17:42:44 +0530

    add callback

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 export default function Home() {
   const [appData, setAppData] = useState('')
   const handleProps = () => { console.log('handleProps'), window.ReactNativeWebView.postMessage(JSON.stringify({ label: 'react-testing' })) }
 
-  useEffect(() => {
-    console.log('UseEffect');
+  const renderAgain = useCallback(() => {
     window.addEventListener("message", (event) => {
+      console.log('INRenderAgainListener', event);
       setAppData(JSON.stringify(event.data));
     })
   }, [])
 
-  useEffect(()=>{
-    console.log({appData});
-  },[appData])
+  useEffect(() => {
+    console.log({ appData });
+  }, [appData])
 
   return (
     <div>
       <h1>Native Testing...</h1>
       <button onClick={handleProps}>Click here</button>
       <h1>{appData}</h1>
+      <button onClick={renderAgain}>Render Again</button>
     </div>
   )
 }
