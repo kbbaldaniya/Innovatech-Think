c12e4eb926b92ae4beb9a771a9ecc9550723cd92
kbbaldaniya <kishanbaldaniya2@gmail.com>
2023-03-01 12:14:53 +0530

    - add listener

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import { useEffect } from "react"
 
 export default function Home() {
-  const handleProps = () => { window.ReactNativeWebView.postMessage(JSON.stringify({ label: 'react-testing' })) }
+  const handleProps = () => { console.log('handleProps'), window.ReactNativeWebView.postMessage(JSON.stringify({ label: 'react-testing' })) }
+
+
+  document.addEventListener("message", function (data) {
+    console.log('data', data);
+    // newData.push(data.data)
+    alert(data.data)
+  })
+
 
   return (
     <div>
