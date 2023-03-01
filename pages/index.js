a21e3233eb59f423bb5a5408e1f41ce46132a4ee
kbbaldaniya <kishanbaldaniya2@gmail.com>
2023-03-01 12:43:32 +0530

    addEventlistener

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
 import { useEffect, useLayoutEffect, useState } from "react"
 
 export default function Home() {
-  const [effectCall, setEffectCall] = useState(true)
   const handleProps = () => { console.log('handleProps'), window.ReactNativeWebView.postMessage(JSON.stringify({ label: 'react-testing' })) }
 
   useEffect(() => {
     console.log('UseEffect');
-
     window.addEventListener("message", (event) => {
       console.log('data', event.data);
-      setEffectCall(!effectCall)
     })
-  }, [effectCall])
+  }, [])
 
 
   return (
