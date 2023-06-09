e51e02ab737b15fdbce0075c70cbd0410904b1a0
Kishan Baldaniya <78065952+kbbaldaniya@users.noreply.github.com>
2023-06-09 17:16:19 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>InnovatechThink Technology</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
