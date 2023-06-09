3241c942870eefbfa1bdac61258054f47dd5a481
Kishan Baldaniya <78065952+kbbaldaniya@users.noreply.github.com>
2023-06-09 17:17:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
-import { useCallback, useEffect, useState } from "react"
+import { useEffect } from "react";
 
 export default function Home() {
-  const [appData, setAppData] = useState('')
-  const handleProps = () => { console.log('handleProps'), window.ReactNativeWebView.postMessage(JSON.stringify({ label: 'react-testing' })) }
-
-  const renderAgain = useCallback(() => {
-    window.addEventListener("message", (event) => {
-      console.log('INRenderAgainListener', event);
-      setAppData(JSON.stringify(event.data));
-    })
-  }, [])
-
-  useEffect(() => {
-    console.log({ appData });
-  }, [appData])
+  useEffect(() =>
+    window.addEventListener("scroll", () => {
+      document.body.style.setProperty("--scroll", window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+    },
+      false
+    ))
 
   return (
-    <div>
-      <h1>Native Testing...</h1>
-      <button onClick={handleProps}>Click here</button>
-      <h1>{appData}</h1>
-      <button onClick={renderAgain}>Render Again</button>
-    </div>
+    <>
+      <div className="loading">
+        <h1>Scroll Down</h1>
+        <center class="down">
+          <div class="ball3"></div>
+          <div class="spacer"></div>
+          <div class="ball2"></div>
+          <div class="spacer"></div>
+          <div class="ball1"></div>
+          <div class="arrow"></div>
+        </center>
+      </div>
+      <svg className="logo" width="100" height="100" viewBox="0 0 24 24">
+        <path d="M21,9H15V22H13V16H11V22H9V9H3V7H21M12,2A2,2 0 0,1 14,4A2,2 0 0,1 12,6C10.89,6 10,5.1 10,4C10,2.89 10.89,2 12,2Z" />
+      </svg>
+      <h2 className="heading-1">Exciting things are happening behind the scenes!</h2>
+      <h3 className="heading-2">Our site is currently under creation and will be ready soon.</h3>
+    </>
   )
 }
