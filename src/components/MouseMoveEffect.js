d14807efc42d574f093f49d6a2025a000641bbfa
Kishan Baldaniya <78065952+kbbaldaniya@users.noreply.github.com>
2023-06-23 19:15:37 +0900

    add svg

---
+++ src/components/MouseMoveEffect.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+
+const MouseMoveEffect = () => {
+  useEffect(() => {
+
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallax");
+
+    function parallax(e) {
+      let _w = window.innerWidth / 2;
+      let _h = window.innerHeight / 2;
+      let _mouseX = e.clientX;
+      let _mouseY = e.clientY;
+      let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+      let _depth3 = `${50 - (_mouseX - _w) * 0.04}% ${50 - (_mouseY - _h) * 0.04}%`;
+      let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+      elem.style.backgroundPosition = x;
+    }
+  }, [])
+
+  return (
+    <div id='parallax-frame'>
+      <div id="parallax"></div>
+    </div>
+  )
+}
+
+export default MouseMoveEffect
